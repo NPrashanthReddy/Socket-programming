267f5616fe3392ca77c9f0120df1543605020163
NPrashanthReddy <nprashanthreddy@outlook.com>
2021-12-10 12:40:01 +0530

    .

--- chat_server.py
+++ chat_server.py
@@ -46,7 +46,7 @@ def receive():
 
 		print(f"User took a name of {username}")
 		broadcast(f"{username} joined the chat!\n".encode('utf-8'))
-		client.send(f"You've justed joined the chat\n Welcome to babel {username}".encode("utf-8"))
+		client.send(f"You've justed joined the chat\n Welcome to babel {username}\n".encode("utf-8"))
 
 		thread = threading.Thread(target=handle, args=(client,))
 		thread.start()
